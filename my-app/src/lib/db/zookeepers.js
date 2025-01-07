a77da93405dec33565f1dd5c10e042bc6c09d70b
Nikola Mitrovic <business.mitrovic@gmail.com>
2025-01-07 01:18:40 +0100

    refactor

--- my-app/src/lib/db/zookeepers.js
+++ my-app/src/lib/db/zookeepers.js
@@ -18,18 +18,15 @@ export async function getZookeepers() {
         console.error('DB: Error getting zookeepers:', error);
         throw error;
     }
-    return zookeepers;
 }
 
 export async function getZookeeper(id) {
     try {
-        console.log('DB: Getting zookeeper with ID:', id);
         const collection = await getCollection("zookeepers");
         const zookeeper = await collection.findOne({ _id: new ObjectId(id) });
         if (zookeeper) {
             zookeeper._id = zookeeper._id.toString();
         }
-        console.log('DB: Found zookeeper:', zookeeper);
         return zookeeper;
     } catch (error) {
         console.error('DB: Error getting zookeeper:', error);
@@ -59,12 +56,9 @@ export async function createZookeeper(zookeeper) {
     try {
         const highestId = await getHighestZookeeperId();
         zookeeper.id = highestId + 1;
-        console.log('DB: Assigning new zookeeper ID:', zookeeper.id);
         
-        console.log('DB: Creating new zookeeper:', zookeeper);
         const collection = await getCollection("zookeepers");
         const result = await collection.insertOne(zookeeper);
-        console.log('DB: Created zookeeper with ID:', result.insertedId);
         return result.insertedId.toString();
     } catch (error) {
         console.error('DB: Error creating zookeeper:', error);
@@ -74,8 +68,6 @@ export async function createZookeeper(zookeeper) {
 
 export async function assignZookeeperToAnimal(animalId, zookeeperId) {
     try {
-        console.log(`DB: Attempting to assign zookeeper ${zookeeperId} to animal ${animalId}`);
-        
         // First verify both animal and zookeeper exist
         const animal = await getAnimal(animalId);
         const zookeeper = await getZookeeper(zookeeperId);
@@ -95,9 +87,6 @@ export async function assignZookeeperToAnimal(animalId, zookeeperId) {
             throw new Error('Animal does not have a valid ID');
         }
         
-        console.log('DB: Found animal:', animal);
-        console.log('DB: Found zookeeper:', zookeeper);
-        
         // Update the zookeeper's animal_id
         const collection = await getCollection("zookeepers");
         const result = await collection.updateOne(
@@ -105,14 +94,10 @@ export async function assignZookeeperToAnimal(animalId, zookeeperId) {
             { $set: { animal_id: animal.id } }
         );
         
-        console.log('DB: Assignment result:', result);
-        
         if (result.modifiedCount === 0) {
-            console.log('DB: No zookeeper was updated');
             throw new Error('Failed to update zookeeper');
         }
         
-        console.log('DB: Successfully assigned zookeeper to animal');
         return true;
     } catch (error) {
         console.error('DB: Error assigning zookeeper to animal:', error);
@@ -122,18 +107,13 @@ export async function assignZookeeperToAnimal(animalId, zookeeperId) {
 
 export async function updateZookeeper(id, updatedData) {
     try {
-        console.log('DB: Updating zookeeper with ID:', id);
-        console.log('DB: Update data:', updatedData);
-        
         const collection = await getCollection("zookeepers");
         
         // Convert string ID to ObjectId
         const objectId = new ObjectId(id);
-        console.log('DB: Converted ObjectId:', objectId);
         
         // Remove _id from update data if it exists
         const { _id, ...updateData } = updatedData;
-        console.log('DB: Cleaned update data:', updateData);
         
         // If we're removing animal_id, use $unset
         let updateOperation;
@@ -142,13 +122,11 @@ export async function updateZookeeper(id, updatedData) {
         } else {
             updateOperation = { $set: updateData };
         }
-        console.log('DB: Update operation:', updateOperation);
         
         const result = await collection.updateOne(
             { _id: objectId },
             updateOperation
         );
-        console.log('DB: Update result:', result);
         
         if (result.matchedCount === 0) {
             throw new Error('No document matched the ID');
